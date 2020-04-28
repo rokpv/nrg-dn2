8c4e84a54cba3d936eabbe37c9e6a8449c0213eb
Rok <rp8257@student.uni-lj.si>
2020-04-28 19:59:13 +0200

    Fixed sphere intersection

--- MaterialsLights/OrenNayar.cs
+++ MaterialsLights/OrenNayar.cs
@@ -1,20 +1,59 @@
+using System;
+
 namespace PathTracer
 {
     public class OrenNayar : BxDF
     {
-        public override Spectrum f(Vector3 wo, Vector3 wi)
+        private Spectrum kd;
+        private double a;
+        private double b;
+
+        private static double Fi(double x, double y)
         {
-            throw new System.NotImplementedException();
+            return Math.Atan(y / x);
         }
 
-        public override (Spectrum, Vector3, double) Sample_f(Vector3 woL)
+        private static double Theta(double x, double y, double z)
         {
-            throw new System.NotImplementedException();
+            return Math.Atan(Math.Sqrt(x * x + y * y) / z);
+        }
+
+        public OrenNayar(Spectrum kd, double roughness)
+        {
+            this.kd = kd;
+            var roughnessSq = roughness * roughness;
+            a = 1 - roughnessSq / 2 * (roughnessSq + 0.33);
+            b = 0.45 * roughnessSq / (roughnessSq + 0.09);
+        }
+
+        public override Spectrum f(Vector3 wo, Vector3 wi)
+        {
+            var fiWi = Fi(wi.x, wi.y);
+            var fiWo = Fi(wo.x, wo.y);
+            var thetaWi = Theta(wi.x, wi.y, wi.z);
+            var thetaWo = Theta(wo.x, wo.y, wo.z);
+            
+            var alpha = Math.Max(thetaWi, thetaWo);
+            var beta = Math.Min(thetaWi, thetaWo);
+
+            return kd * Utils.PiInv * (a + b * Math.Max(0, Math.Cos(fiWi - fiWo))) * Math.Sin(alpha) * Math.Sin(beta);
+        }
+
+        public override (Spectrum, Vector3, double) Sample_f(Vector3 wo)
+        {
+            var wi = Samplers.CosineSampleHemisphere();
+            if (wo.z < 0)
+                wi.z *= -1;
+            double pdf = Pdf(wo, wi);
+            return (f(wo, wi), wi, pdf);
         }
 
         public override double Pdf(Vector3 wo, Vector3 wi)
-        {
-            throw new System.NotImplementedException();
+        { 
+            if (!Utils.SameHemisphere(wo, wi))
+                return 0;
+
+            return Math.Abs(wi.z) * Utils.PiInv; // wi.z == cosTheta
         }
     }
 }
--- Primitives/Sphere.cs
+++ Primitives/Sphere.cs
@@ -17,8 +17,6 @@ namespace PathTracer
     {
       Ray ray = WorldToObject.Apply(r);
 
-      // TODO: Compute quadratic sphere coefficients
-      // TODO: Initialize _double_ ray coordinate values
       var ox = ray.o.x;
       var oy = ray.o.y;
       var oz = ray.o.z;
@@ -31,13 +29,11 @@ namespace PathTracer
       var b = 2 * (dx * ox + dy * oy + dz * oz);
       var c = ox * ox + oy * oy + oz * oz - Radius * Radius;
       
-      // TODO: Solve quadratic equation for _t_ values
       var (solvable, t0, t1) = Utils.Quadratic(a, b, c);
       if (!solvable) return (null, null);
-      // TODO: Check quadric shape _t0_ and _t1_ for nearest intersection
+
       var shapeHit = t0 <= 0 ? t1 : t0;
-      
-      // TODO: Compute sphere hit position and $\phi$
+
       var hit = ray.Point(shapeHit);
       hit *= Radius / hit.Length();
 
@@ -47,21 +43,12 @@ namespace PathTracer
       }
 
       var phi = Math.Atan2(hit.y, hit.x);
-      var theta = Math.Acos((hit.z / Radius).Clamp(-1, 1));
       if (phi < 0) phi += 2 * Math.PI;
-      
-      // TODO: Return shape hit and surface interaction
-      var zRadius = Math.Sqrt(hit.x * hit.x + hit.y * hit.y);
-      var invZRadius = 1 / zRadius;
-      var cosPhi = hit.x * invZRadius;
-      var sinPhi = hit.y * invZRadius;
-      
+
       var dpdu = new Vector3(-phi * hit.y, phi * hit.x, 0);
-      var dpdv = new Vector3(hit.z * cosPhi, hit.z * sinPhi, -Radius * Math.Sin(theta));
-      var normal = Vector3.Cross(dpdu, dpdv).Normalize();
-      
-      var interaction = new SurfaceInteraction(hit, normal, -ray.d, dpdu, this);
-      return (shapeHit, interaction);
+
+      var interaction = new SurfaceInteraction(hit, hit, -ray.d, dpdu, this);
+      return (shapeHit, ObjectToWorld.Apply(interaction));
     }
 
     public override (SurfaceInteraction, double) Sample()
@@ -79,17 +66,7 @@ namespace PathTracer
 
     public override double Pdf(SurfaceInteraction si, Vector3 wi)
     {
-      var radiusSq = Radius * Radius;
-      var center = ObjectToWorld.ApplyPoint(new Vector3(0, 0, 0));
-      var origin = si.Point;
-      var diff = origin - center;
-      if (diff.LengthSquared() <= radiusSq)
-      {
-        return base.Pdf(si, wi);
-      }
-
-      // TODO: change
-      return base.Pdf(si, wi);
+      return 1 / Area();
     }
 
   }
